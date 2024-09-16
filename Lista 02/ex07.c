58b8d4411bf6dc2b4942ea9716ba29259fe93a77
Reinan Argolo <reinanlopes.1@gmail.com>
2024-09-16 17:21:06 -0300

    ex07

---
+++ Lista 02/ex07.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char r = 'R';
+
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c\n", r);
+    printf("%c\t", r);
+
+    printf("%c\n", r);
+     printf("%c\t", r);
+
+    printf("%c\n", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("%c", r);
+    printf("\n%c", r);
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf("%c", r);
+
+    printf("\n%c\n", r);
+    printf("%c", r);
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf("%c", r);
+    
+    
+}
