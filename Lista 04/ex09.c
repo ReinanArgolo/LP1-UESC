749c58da4500e03e30be3d97a848be39389ef083
Reinan Argolo <reinanlopes.1@gmail.com>
2024-10-21 13:58:11 -0300

    ex09

---
+++ Lista 04/ex09.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+
+int sortearNumero();
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    int numRand, chute;
+
+    srand(time(NULL));
+    numRand = sortearNumero();
+
+    printf("- - - - JOGO DE ADIVINHAÇÃO - - - -\n");
+    printf("Tente acertar o número sorteado: ");
+    scanf("%d", &chute);
+
+    if (chute == numRand) {
+        printf("Excelente! Você adivinhou o numero\n");
+    }
+    
+    while(chute != numRand) {
+        printf("Bem, nâo e isso. Tente acertar o número sorteado: ");
+        scanf("%d", &chute);
+
+        if(chute == numRand) {
+            printf("Excelente! Você adivinhou o numero\n");
+        } else if (chute > numRand) {
+            printf("Muito Alto. Tente novamente\n");
+        } else {
+            printf("Muito Baixo. Tente novamente\n");
+        }
+    }
+}
+
+int sortearNumero() {
+    return 1 + (rand() % 100);
+}
