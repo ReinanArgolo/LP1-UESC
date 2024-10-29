afc90edb4778a226f8174c17082988dbae00717c
Reinan Argolo <reinanlopes.1@gmail.com>
2024-10-29 20:58:22 -0300

    ex 03 - lista 4.1

--- Lista 04-1/ex03.c
+++ Lista 04-1/ex03.c
@@ -3,15 +3,22 @@
 int ordenarNumeros(int n1, int n2, int n3);
 
 int main() {
-    int n1, n2, n3;
-    printf("Digite o primeiro número: ");
-    scanf("%d", &n1);
-    printf("Digite o segundo número: ");
-    scanf("%d", &n2);
-    printf("Digite o terceiro número: ");
-    scanf("%d", &n3);
+    int n1, n2, n3, triplas;
 
-    ordenarNumeros(n1, n2, n3);
+    printf("digite a quantidade de triplas: ");
+    scanf("%d", &triplas);
+
+    for (int i = 0; i < triplas; i++) {
+        printf("Digite o primeiro número: ");
+        scanf("%d", &n1);
+        printf("Digite o segundo número: ");
+        scanf("%d", &n2);
+        printf("Digite o terceiro número: ");
+        scanf("%d", &n3);
+
+        ordenarNumeros(n1, n2, n3);
+    }
+    
 }
 
 int ordenarNumeros(int n1, int n2, int n3) {
@@ -20,7 +27,7 @@ int ordenarNumeros(int n1, int n2, int n3) {
     
 
     int aux;
-    if(n1 > n2) {
+    
         if (n2 > n3) {
             aux = n1;
             n1 = n2;
@@ -42,8 +49,14 @@ int ordenarNumeros(int n1, int n2, int n3) {
             n3 = n2;
             n2 = aux;
         }
+
+        if (n1 > n2) {
+            aux = n2;
+            n2 = n1;
+            n1 = aux;
+        }
         
-    } 
+    
 
     printf("n1: %d, n2: %d, n3: %d\n", n1, n2, n3);
 }
