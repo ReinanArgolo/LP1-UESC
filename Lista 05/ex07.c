fea7791e1dba9fe62cd04f241a20930db78fa577
Reinan Argolo <reinanlopes.1@gmail.com>
2024-11-15 11:27:13 -0300

    teste de eficiencia ex 07

--- Lista 05/ex07.c
+++ Lista 05/ex07.c
@@ -13,19 +13,24 @@ int main(void) {
 
     clock_t start, end;
     double cpu_time_used;
-
     start = clock();
 
     preencherVetor(vetor1, vetor2);
+
     for (int i = 0; i < 800; i++) {
         reorganizarVetor(vetor1, vetor2, vetor3);
     }
 
     imprimirVetor(vetor3);
-    end = clock();
 
+    end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Tempo de execucao: %f segundos\n", cpu_time_used);
+
+    // Usando o (int)(i*0.5), rodou em 0.005000s
+    // Usando o i/2, rodou em 0.011000
+
+    // A função foi executada 800x
 
 }
 
@@ -42,18 +47,16 @@ void preencherVetor(int vetor1[], int vetor2[]) {
 }
 
 void reorganizarVetor(int vetor1[], int vetor2[], int vetor3[]) {
-
-
-
+    
    for(int i = 0; i < SIZE*2; i++) {
-    vetor3[i] = vetor1[(int)(i*0.5)] * (1 - i%2) + vetor2[(int)(i*0.5)] * (i%2);
+        vetor3[i] = vetor1[(int)(i*0.5)] * (1 - i%2) + vetor2[(int)(i*0.5)] * (i%2);
    }
 
-//    for(int i = 0; i < SIZE*2; i++) {
-//     vetor3[i] = vetor1[i/2] * (1 - i%2) + vetor2[i/2] * (i%2);
-//    }
+    //   for(int i = 0; i < SIZE*2; i++) {
+    //     vetor3[i] = vetor1[i/2] * (1 - i%2) + vetor2[i/2] * (i%2);
+    //    }
 
-   // Nesta função, se o meu i for par i%2 será 0, logo 1 - 0 = 1, inserindo o vetor i na posição [i/2] (pois é necessário corrigir por conta de percorrer 40 posições sendo que há apenas 20 nos vetores "base"), para impar a lógica inversa
+   // Nesta função, se o meu i for par i%2 será 0, logo 1 - 0 = 1, inserindo o vetor1 na pos i o vetor na pos [i/2] (pois é necessário corrigir por conta de percorrer 40 posições sendo que há apenas 20 nos vetores "base"), para impar a lógica inversa
 
 }
 
